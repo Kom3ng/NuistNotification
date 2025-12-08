fb3174b68d7d30b688248e667ab6e37aae3c6d2e
ast <99592850+Kom3ng@users.noreply.github.com>
2025-12-08 16:35:47 +0800

    feat: add pending intent to notification for detail activity

--- app/src/main/java/moe/okay/nuistnotification/SettingsActivity.kt
+++ app/src/main/java/moe/okay/nuistnotification/SettingsActivity.kt
@@ -323,13 +323,23 @@ fun showSystemNotification(context: Context, news: News) {
         description = "当有新数据时通知"
     }
     notificationManager.createNotificationChannel(channel)
-
+    val intent = Intent(context, DetailActivity::class.java).apply {
+        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        putExtra("target", Json.encodeToString(news))
+    }
+    val pendingIntent = PendingIntent.getActivity(
+        context,
+        0,
+        intent,
+        PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+    )
     val notification = NotificationCompat.Builder(context, channelId)
         .setSmallIcon(R.drawable.ic_launcher_foreground)
         .setContentTitle(news.title)
         .setContentText(news.summary)
         .setPriority(NotificationCompat.PRIORITY_DEFAULT)
         .setAutoCancel(true)
+        .setContentIntent(pendingIntent)
         .build()
 
     notificationManager.notify(notificationId, notification)
